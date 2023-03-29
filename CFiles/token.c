b5e7ed743f015288cf63bf9a0247025392e3ef33
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2023-03-29 12:10:21 +0200

    token

--- CFiles/token.c
+++ CFiles/token.c
@@ -1,6 +1,6 @@
 #include "../Include/minishell.h"
 
-int	exclued_char(char c)
+int	excluded_char(char c)
 {
 	if (c == 9 || c == 32)
 		return (1);
@@ -15,8 +15,14 @@ int	get_total_token(char *str)
 
 	total = 0;
 	i = 0;
+	if (excluded_char(str[0]) == 0 && str[i] != endline)
+		total ++;
 	while (str[i] != endline)
 	{
+		if (excluded_char(str[i]) == 1)
+			i ++;
+		if (excluded_char(str[i]) == 0 && excluded_char(str[i - 1]) == 1 && str[i] != endline)
+			total ++;
 		i ++;
 	}
 	return (total);
@@ -24,5 +30,11 @@ int	get_total_token(char *str)
 
 void	get_token(t_token **token, char *str)
 {
-	printf("%d", get_total_token(str));
+	int i;
+
+	i = 0;
+	while (i < get_total_token(str))
+	{
+		
+	}
 }
