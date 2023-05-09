97ccdf0b9040fde272bd2d445b1d686650b5d0b9
Theo Bouverat <tbouvera@student.42lausanne.ch>
2023-05-09 14:23:32 +0200

    updatelist

--- Include/minishell.h
+++ Include/minishell.h
@@ -17,6 +17,23 @@ typedef struct s_token
 	struct s_token	*next;
 } t_token;
 
+typedef struct s_exec2
+{
+    char            **cmd_args;
+    struct t_redir    *redir;
+    struct s_exec    *next;
+    struct s_exec    *prev;
+}    t_exec;
+
+typedef struct s_redir
+{
+    char            *file;
+    char            *Heredoc_args;
+    struct s_redir    *next;
+    struct s_redir    *prev;
+    int                cases;
+}    t_redir;
+
 void	free_list(void	**list);
 void	init_token(t_token **token, char *str);
 t_token	*create_new();
