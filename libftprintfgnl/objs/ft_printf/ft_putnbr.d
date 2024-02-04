91254aab79b5c0e021cab1a4a985b1bbb2473a0c
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2024-02-04 14:12:17 +0100

    update

--- libftprintfgnl/objs/ft_printf/ft_putnbr.d
+++
@@ -1,3 +0,0 @@
-objs/ft_printf/ft_putnbr.o: srcs/ft_printf/ft_putnbr.c includes/libft.h
-
-includes/libft.h:
--- libftprintfgnl/objs/get_next_line/get_next_line_bonus.d
+++
@@ -1,4 +0,0 @@
-objs/get_next_line/get_next_line_bonus.o: \
-  srcs/get_next_line/get_next_line_bonus.c includes/libft.h
-
-includes/libft.h:
